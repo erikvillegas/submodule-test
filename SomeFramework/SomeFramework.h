dc37edb871968d2c8b7ffd335f4c3fa6a1806557
Erik Villegas <erik.villegas@carvana.com>
2017-09-23 15:25:15 -0700

    initial commit

---
+++ SomeFramework/SomeFramework.h
@@ -0,0 +1,19 @@
+//
+//  SomeFramework.h
+//  SomeFramework
+//
+//  Created by Erik Villegas on 9/23/17.
+//  Copyright © 2017 Carvana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SomeFramework.
+FOUNDATION_EXPORT double SomeFrameworkVersionNumber;
+
+//! Project version string for SomeFramework.
+FOUNDATION_EXPORT const unsigned char SomeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SomeFramework/PublicHeader.h>
+
+
